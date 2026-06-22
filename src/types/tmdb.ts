6c495e48b33f5e4558e23219dfa268331f6bbed0
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-06-22 19:55:06 +0300

    feat(types): add TMDBCompany, TMDBCompanyDetails, TMDBCompanyMovie types

--- src/types/tmdb.ts
+++ src/types/tmdb.ts
@@ -435,6 +435,41 @@ export type TVSeasonDetails = {
   episodes: TVEpisode[];
 };
 
+export type TMDBCompany = {
+  id: number;
+  name: string;
+  logo_path: string | null;
+  origin_country: string;
+};
+
+export type TMDBCompanyDetails = {
+  id: number;
+  name: string;
+  logo_path: string | null;
+  origin_country: string;
+  headquarters: string;
+  homepage: string;
+  description: string;
+  parent_company: TMDBCompany | null;
+};
+
+export type TMDBCompanyMovie = {
+  id: number;
+  title: string;
+  overview: string;
+  poster_path: string | null;
+  backdrop_path: string | null;
+  release_date: string;
+  vote_average: number;
+  vote_count: number;
+  genre_ids: number[];
+  original_language: string;
+  original_title: string;
+  popularity: number;
+  adult: boolean;
+  video: boolean;
+};
+
 export type TMDBResponse<T> = {
   page: number;
   results: T[];
