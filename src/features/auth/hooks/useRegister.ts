683094509017f5ade9f6619070b1f33d029f90bc
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-06-22 16:25:38 +0300

    feat: implement useRegister hook for user registration with Firebase

---
+++ src/features/auth/hooks/useRegister.ts
@@ -0,0 +1,44 @@
+"use client";
+
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth } from "@/lib/firebase";
+import { useState } from "react";
+
+type RegisterData = {
+    name: string;
+    email: string;
+    password: string;
+};
+
+export default function useRegister() {
+    const [firebaseError, setFirebaseError] = useState<string | null>(null);
+
+    const register = async (data: RegisterData) => {
+        try {
+            const response = await createUserWithEmailAndPassword(
+                auth,
+                data.email,
+                data.password
+            );
+
+            await updateProfile(response.user, {
+                displayName: data.name,
+            });
+
+            return response;
+        } catch (error: any) {
+            const firebaseErr =
+                error?.code?.replace("auth/", "") ||
+                error?.message ||
+                "Unknown error";
+
+            setFirebaseError(firebaseErr);
+            return null;
+        }
+    };
+
+    return {
+        register,
+        firebaseError,
+    };
+}
