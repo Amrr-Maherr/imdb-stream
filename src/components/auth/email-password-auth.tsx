c9922802e83d38d34871158be9fae9f0afd27654
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-06-22 16:08:25 +0300

    feat: enhance EmailPasswordAuth component with form validation and password visibility toggle

--- src/components/auth/email-password-auth.tsx
+++ src/components/auth/email-password-auth.tsx
@@ -1,13 +1,43 @@
-import { Mail, Eye } from "lucide-react";
+"use client";
+import { Mail, Eye, EyeOff } from "lucide-react";
 import Link from "next/link";
-
+import { useForm, SubmitHandler } from "react-hook-form";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import useLogin from "@/features/auth/hooks/useLogin";
+import AuthStatusMessage from "@/features/auth/components/AuthStatusMessage";
 
 export function EmailPasswordAuth() {
+  const { login, firebaseError } = useLogin();
+  const [successMessage, setSuccessMessage] = useState<string | null>(null);
+  type Inputs = {
+    email: string;
+    password: string;
+  };
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitting },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    try {
+      const result = await login(data);
+      console.log(result?.user);
+      if (result?.user) {
+        setSuccessMessage("Welcome back! You’ve signed in successfully.");
+        reset();
+      }
+    } catch (error: any) {
+      console.log(error);
+    }
+  };
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
-    <div className="space-y-4">
+    <form className={`space-y-4 `} onSubmit={handleSubmit(onSubmit)}>
       <div className="space-y-2">
         <Label htmlFor="auth-email">Email</Label>
         <div className="relative">
@@ -15,13 +45,24 @@ export function EmailPasswordAuth() {
             id="auth-email"
             type="email"
             placeholder="you@example.com"
-            className="peer pe-9"
+            className={`peer pe-9 ${errors?.email ? "border-red-700" : ""}`}
             autoComplete="email"
+            {...register("email", {
+              required: "Email is required",
+            })}
           />
+
           <div className="pointer-events-none absolute inset-y-0 end-0 flex items-center justify-center pe-3 text-muted-foreground/80 peer-disabled:opacity-50">
             <Mail size={16} strokeWidth={2} aria-hidden="true" />
           </div>
         </div>
+        {errors.email?.message && (
+          <AuthStatusMessage
+            message={errors.email.message || null}
+            type="error"
+            className="mt-2 text-sm text-red-600"
+          />
+        )}
       </div>
 
       <div className="space-y-2">
@@ -37,27 +78,59 @@ export function EmailPasswordAuth() {
         <div className="relative">
           <Input
             id="auth-password"
-            type="password"
+            type={`${showPassword ? "text" : "password"}`}
             placeholder="Enter your password"
-            className="peer pe-9"
+            className={`peer pe-9 ${errors.password ? "border-red-700" : ""}`}
             autoComplete="current-password"
+            {...register("password", {
+              required: "password is required",
+            })}
           />
+
           <button
+            onClick={() => {
+              setShowPassword(!showPassword);
+            }}
             type="button"
             className="absolute inset-y-0 end-0 flex items-center justify-center pe-3 text-muted-foreground/80 hover:text-foreground transition-colors"
             tabIndex={-1}
             aria-label="Show password"
           >
-            <Eye size={16} strokeWidth={2} aria-hidden="true" />
+            {showPassword ? (
+              <EyeOff size={16} strokeWidth={2} aria-hidden="true" />
+            ) : (
+              <Eye size={16} strokeWidth={2} aria-hidden="true" />
+            )}
           </button>
         </div>
+        {errors.password && (
+          <AuthStatusMessage
+            message={errors.password.message || null}
+            type="error"
+            className="mt-2 text-sm text-red-600"
+          />
+        )}
       </div>
-
+      {firebaseError && (
+        <AuthStatusMessage
+          message={firebaseError || null}
+          type="error"
+          className="mt-2 text-sm text-red-600"
+        />
+      )}
+      {successMessage && (
+        <AuthStatusMessage
+          message={successMessage || null}
+          type="success"
+          className="mt-2 text-sm text-green-600"
+        />
+      )}
       <Button
         type="submit"
-        className="w-full bg-brand text-brand-foreground hover:bg-brand/90 font-semibold"
+        disabled={isSubmitting}
+        className="w-full bg-brand text-brand-foreground hover:bg-brand/90 font-semibold disabled:opacity-70 disabled:cursor-not-allowed"
       >
-        Sign In
+        {isSubmitting ? "Signing in..." : "Sign in"}
       </Button>
 
       <p className="text-center text-sm text-muted-foreground">
@@ -69,6 +142,6 @@ export function EmailPasswordAuth() {
           Create your account
         </Link>
       </p>
-    </div>
+    </form>
   );
 }
