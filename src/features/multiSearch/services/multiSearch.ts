d0aea25c10564ad952a0ebcd49b3f7fa278c943a
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-06-26 20:21:11 +0300

    feat(multi-search): create service for TMDB multi search

---
+++ src/features/multiSearch/services/multiSearch.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+type MultiSearchProps = {
+    query: string
+}
+export default async function MultiSearch({ query }: MultiSearchProps) {
+    try {
+        const response = await axios.get("https://api.themoviedb.org/3/search/multi", {
+            params: {
+                query,
+                language: "en-US",
+                page: 1,
+                include_adult: false,
+                api_key: process.env.NEXT_PUBLIC_TMDB_API_KEY,
+            }
+        })
+        return response.data
+    } catch (error) {
+        console.log(error);
+    }
+}
